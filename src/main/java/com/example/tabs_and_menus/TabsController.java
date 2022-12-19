163854deb079a9e84a9082639d736ca6ac83f930
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-12-19 13:41:13 -0500

    tabs with buttons

--- src/main/java/com/example/tabs_and_menus/TabsController.java
+++ src/main/java/com/example/tabs_and_menus/TabsController.java
@@ -1,4 +1,47 @@
 package com.example.tabs_and_menus;
 
-public class TabsController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TabsController implements Initializable {
+
+
+    public AnchorPane rootPane;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        BorderPane borderPane= new BorderPane();
+        TabPane tabPane = new TabPane();
+
+        Tab tab1 = new Tab("First");
+        Tab tab2 = new Tab("Second");
+        Tab tab3 = new Tab("Third");
+
+        tab1.setClosable(false);
+        tab2.setClosable(false);
+        tab3.setClosable(false);
+
+        Button btn1 = new Button("Function 1");
+        Button btn2 = new Button("Function 2");
+        Button btn3 = new Button("Function 3");
+
+        tab1.setContent(btn1);
+        tab2.setContent(btn2);
+        tab3.setContent(btn3);
+
+        tabPane.getTabs().addAll(tab1,tab2,tab3);
+        borderPane.setCenter(tabPane);
+
+        rootPane.getChildren().add(borderPane);
+
+
+    }
 }
