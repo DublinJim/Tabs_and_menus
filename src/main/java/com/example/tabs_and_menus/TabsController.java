6ee5f55f68c8f2db809b3a6d35c05c9f2f69fff7
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-12-19 15:09:09 -0500

    checkbox added

--- src/main/java/com/example/tabs_and_menus/TabsController.java
+++ src/main/java/com/example/tabs_and_menus/TabsController.java
@@ -2,10 +2,13 @@ package com.example.tabs_and_menus;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,11 +17,14 @@ public class TabsController implements Initializable {
 
 
     public AnchorPane rootPane;
+    public Text txt1;
+    public CheckBox checkBox;
 
+    public Boolean isOn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        BorderPane borderPane= new BorderPane();
+        BorderPane borderPane = new BorderPane();
         TabPane tabPane = new TabPane();
 
         Tab tab1 = new Tab("First");
@@ -33,15 +39,40 @@ public class TabsController implements Initializable {
         Button btn2 = new Button("Function 2");
         Button btn3 = new Button("Function 3");
 
+
+        checkBox = new CheckBox("Always on top");
+        setOn();
+        btn1.setOnAction(e -> settingText());
         tab1.setContent(btn1);
+
+        VBox vBox = new VBox(50.0, btn1, checkBox);
+
+        tab1.setContent(vBox);
+
         tab2.setContent(btn2);
         tab3.setContent(btn3);
 
-        tabPane.getTabs().addAll(tab1,tab2,tab3);
+
+        tabPane.getTabs().addAll(tab1, tab2, tab3);
         borderPane.setCenter(tabPane);
 
         rootPane.getChildren().add(borderPane);
 
 
+    }
+
+    public void setOn() {
+        if (checkBox.isSelected()) {
+            txt1.setText("Selected");
+            System.out.println(isOn);
+        } else {
+            txt1.setText("is off");
+            isOn = false;
+            System.out.println(isOn);
+        }
+    }
+
+    public void settingText() {
+        txt1.setText("Button 1 pressed");
     }
 }
